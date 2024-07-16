63f1b3c7ecee29e6460ac1a646c9ee292052dbc8
admsmn02 <adam.simon@edu.devinci.fr>
2024-07-16 10:17:26 +0200

    :hammer: created entities

---
+++ src/Entity/Commande.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommandeRepository::class)]
+class Commande
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $createdDate = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $orderedDrinks = [];
+
+    #[ORM\Column]
+    private ?int $tableNumber = null;
+
+    #[ORM\ManyToOne(inversedBy: 'commandes')]
+    private ?User $waiter = null;
+
+    #[ORM\ManyToOne(inversedBy: 'commandes')]
+    private ?User $barman = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedDate(): ?\DateTimeInterface
+    {
+        return $this->createdDate;
+    }
+
+    public function setCreatedDate(\DateTimeInterface $createdDate): static
+    {
+        $this->createdDate = $createdDate;
+
+        return $this;
+    }
+
+    public function getOrderedDrinks(): array
+    {
+        return $this->orderedDrinks;
+    }
+
+    public function setOrderedDrinks(array $orderedDrinks): static
+    {
+        $this->orderedDrinks = $orderedDrinks;
+
+        return $this;
+    }
+
+    public function getTableNumber(): ?int
+    {
+        return $this->tableNumber;
+    }
+
+    public function setTableNumber(int $tableNumber): static
+    {
+        $this->tableNumber = $tableNumber;
+
+        return $this;
+    }
+
+    public function getWaiter(): ?User
+    {
+        return $this->waiter;
+    }
+
+    public function setWaiter(?User $waiter): static
+    {
+        $this->waiter = $waiter;
+
+        return $this;
+    }
+
+    public function getBarman(): ?User
+    {
+        return $this->barman;
+    }
+
+    public function setBarman(?User $barman): static
+    {
+        $this->barman = $barman;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
